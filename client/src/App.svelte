8438fa524d770d1f0c7efca7f658663ddfe93520
ogun.babacan <ogun.babacan@trendyol.com>
2020-08-19 00:49:00 +0300

    feat: client

---
+++ client/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<main>
+  <form>
+    <label>
+      Username:
+      <input type="text" placeholder="Username" />
+    </label>
+    <label>
+      Password:
+      <input type="password" placeholder="Password" />
+    </label>
+    <button>CONNECT</button>
+  </form>
+</main>
