679c4cebd9dac484b1825b6a8147d453c210cbaa
Ogun Babacan <babacanogun@gmail.com>
2020-09-06 18:04:37 +0300

    remove log.

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,5 +1,4 @@
 export function getRTCConfig() {
-    console.log(process);
     const ICE_SERVERS = JSON.parse(process.env.ICE_SERVERS);
     const configuration = Object.freeze({
         iceServers: ICE_SERVERS,
